2afcdd9ac2bbd527ea29b39e317cdefacfef50a3
mrdm026M <mrdm026@gmail.com>
2021-10-07 18:51:20 +0530

    layout file added

---
+++ components/Layout.js
@@ -0,0 +1,14 @@
+import { Meta } from "./Meta";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Meta />
+      <div>
+        <main>{children}</main>
+      </div>
+    </>
+  );
+};
+
+export default Layout;
