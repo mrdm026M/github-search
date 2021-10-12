fa536fc4a7b9a25a1ac26ba91b5536bbc8b4779f
mrdm026M <mrdm026@gmail.com>
2021-10-13 00:22:32 +0530

    input files added

---
+++ components/Input/ReposInput.js
@@ -0,0 +1,12 @@
+import SubPara from "../StyledComponents/SubPara";
+
+const ReposInput = () => {
+  return (
+    <div className="flex flex-col items-center">
+      <h1>Input Repos</h1>
+      <SubPara />
+    </div>
+  );
+};
+
+export default ReposInput;
---
+++ components/Input/UserInput.js
@@ -0,0 +1,12 @@
+import SubPara from "../StyledComponents/SubPara";
+
+const UserInput = () => {
+  return (
+    <div className="flex flex-col items-center">
+      <h1>Input User</h1>
+      <SubPara />
+    </div>
+  );
+};
+
+export default UserInput;
