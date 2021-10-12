872d22dcf19289e126d23c45d061f49249633f16
mrdm026M <mrdm026@gmail.com>
2021-10-13 00:21:36 +0530

    add custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class", // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      montserrat: ["Montserrat", "sans-serif"],
+      roboto: ["Roboto", "serif"], // Ensure fonts with spaces have " " surrounding it.
+    },
     extend: {
       screens: {
         sm: "480px",
