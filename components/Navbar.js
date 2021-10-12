8041dfe55dfb02f6b0dc5cb84ec20380b54ed566
mrdm026M <mrdm026@gmail.com>
2021-10-13 00:22:58 +0530

    ui complete

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,24 +1,27 @@
 import { VscGithubInverted } from "react-icons/vsc";
 import Link from "next/link";
 
-const navigation = [{ name: "Git Jobs", href: "#" }];
+const navigation = [
+  { name: "Github", href: "https://github.com" },
+  { name: "Git Jobs", href: "#" },
+];
 
 const Navbar = () => {
   return (
     <>
-      <header className="px-16 py-6">
+      <header className="px-16 py-4">
         <nav className="flex items-center justify-between px-8 py-2">
           <div>
             <Link href="/" passHref>
               <VscGithubInverted className="text-4xl" />
             </Link>
           </div>
-          <div className="navbar">
+          <div className="flex items-center gap-8 navbar">
             {navigation.map((items) => (
               <a
                 key={items.name}
                 href={items.href}
-                className="px-6 py-2 text-lg font-medium border-2 border-gray-700 rounded-md hover:bg-gray-700 hover:text-white"
+                className="px-6 py-2 text-lg font-semibold rounded-md font-montserrat hover:bg-gray-700 hover:text-white"
               >
                 {items.name}
               </a>
