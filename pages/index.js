65efea929ffeaf1ff4c96ceaccc6ceb8a20b13cd
mrdm026M <mrdm026@gmail.com>
2021-10-13 00:22:49 +0530

    hero js added

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,9 @@
-import Tabs from "../components/Tabs";
+import Hero from "../components/Hero";
 
 export default function Home() {
   return (
-    <div className="border-8 border-yellow-700">
-      <h1>
-        <a href="https://nextjs.org">Github Search</a>
-      </h1>
-
-      <p>
-        <code>first next.js project</code>
-      </p>
-
-      <Tabs />
+    <div className="flex flex-col items-center justify-center px-16 py-6">
+      <Hero />
     </div>
   );
 }
